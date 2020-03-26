9726bba9b79b5c744367397bcedeeac8aea759c8
胡智宇 <huzhiyu2828@gmail.com>
2020-03-26 18:24:02 +0800

    init commit

---
+++ Notes/算法/剑指offer/test.cpp
@@ -0,0 +1,16 @@
+// for testing code
+#include <iostream>
+using namespace std;
+int fibonacci(int n){
+    if(n==0){ return 0;}
+    if(n==1){ return 1;}
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+int main(){
+    int n = 10;
+    int result;
+    result = fibonacci(n);
+    cout << result << endl;
+    cout << sizeof(int) << endl;
+}
