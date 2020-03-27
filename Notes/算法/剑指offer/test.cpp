35c604102724d60ab09efbf3819cc8f684bbddf6
胡智宇 <huzhiyu2828@gmail.com>
2020-03-28 02:36:39 +0800

    init commit

--- Notes/算法/剑指offer/test.cpp
+++ Notes/算法/剑指offer/test.cpp
@@ -1,12 +1,14 @@
 // for testing code
 #include <iostream>
 using namespace std;
-// int fibonacci(int n){
-//     if(n==0){ return 0;}
-//     if(n==1){ return 1;}
-//     return fibonacci(n-1) + fibonacci(n-2);
-// }
-long long fibonacci(int n){
+
+/* fibonacci problem
+int fibonacci_1(int n){
+    if(n==0){ return 0;}
+    if(n==1){ return 1;}
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+long long fibonacci_2(int n){
     long long result = 0;
     long long temp_1 = 0;
     long long temp_2 = 1;
@@ -22,9 +24,10 @@ long long fibonacci(int n){
     cout << result << endl;
     return result;
 }
+*/
+
 int main(){
-    int n = 0;
-    int result;
-    result = fibonacci(n);
-    cout << result << endl;
+    int n = 2;
+    cout << "n<<2:" << (n<<1) << endl;
+    cout << "n<<=2:" << (n <<= 1) << endl;
 }
