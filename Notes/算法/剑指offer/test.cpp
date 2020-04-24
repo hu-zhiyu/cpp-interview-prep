5a7e6b51d75e3dd0d5610eee3ee969ffdd3185c0
胡智宇 <huzhiyu2828@gmail.com>
2020-04-24 18:49:05 +0800

    字符串的排列组合

--- Notes/算法/剑指offer/test.cpp
+++ Notes/算法/剑指offer/test.cpp
@@ -5,42 +5,21 @@ using namespace std;
 
 class Solution{
 public:
-    vector<string> result;
-    int begin = 0;
-    vector<string> Combination(string str){
-        if(str.empty()){
-            return result;
-        }
-        Combination(str, result, begin);
-        return result;
-    }
-    void Combination(string str, vector<string> & result, int begin){
-        // 终止条件
-        if(begin == str.size()-1){
-            cout << "terminated" << endl;
-            string last(1, str[begin]);
-            result.push_back(last);
-        }
-        else{
-            Combination(str, result, begin+1);
-            int result_length = result.size();
-            vector<string> temp = result;
-            cout << "testing" << endl;
-            string s(1, str[begin]);
-            for(int i=0; i<result_length; i++){
-                result.push_back(s+temp[i]);
-            }
-            result.push_back(s);
-        }           
-    }
+    vector<vector<int> > chessboard;
+    // 初始化为全0
+    // for(auto chessboard[i]: chessboard){
+    //     chessboard[i] = 0;
+    // }
+    // for(int i=0; i<8; i++){
+    //     for(int j=0; i<8; i++){
+    //         chessboard[i][j] = 0;
+    //     }
+    // }
 };
 
 int main(){
-    vector<string> result;
     Solution a;
-    result = a.Combination("abcd");
-    for(int i=0; i<result.size(); i++){
-        cout << result[i] << endl;
-    }
-    cout << "total numbers: " << result.size();
+    vector<vector<int> > chessboard;
+    chessboard = a.chessboard;
+    cout << chessboard;
 }
