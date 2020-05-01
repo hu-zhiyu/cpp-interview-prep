6b7c6bd4318309264b9aaf3e4ced92747ea4c5b4
胡智宇 <huzhiyu2828@gmail.com>
2020-05-02 05:25:38 +0800

    update_2020_05_02

--- Notes/算法/剑指offer/test.cpp
+++ Notes/算法/剑指offer/test.cpp
@@ -1,34 +1,41 @@
 // for testing code
 #include <iostream>
 #include <vector>
-#include <math.h>
 using namespace std;
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        int len1 = text1.size();
-        int len2 = text2.size();
-        vector<vector<int> > len(len1+1, vector<int>(len2+1, 0));
-        for(int i=1; i<len1+1; i++){
-            for(int j=1; j<len2+1; j++){
-                if(text1[i-1]==text2[j-1]){
-                    len[i][j] = len[i-1][j-1] + 1;
+    int lengthOfLongestSubstring(string s) {
+        int len = s.size();
+        if(len==0){
+            return 0;
+        }
+        if(len==1){
+            return 1;
+        }
+        int win_size = 0;
+        for(int i=0; i<len; i++){
+            for(int j=i; j<len; j++){
+                if(s.find(s[j+1], i)<=j && s.find(s[j+1], i)>=i ){
+                    if(j-i+1 > win_size){
+                        win_size = j-i+1;
+                    }
+                    break;
                 }
-                else{
-                    len[i][j] = len[i-1][j] > len[i][j-1] ? len[i-1][j] : len[i][j-1];
+                if(j-i+1 > win_size){
+                    win_size = j-i+1;
                 }
             }
         }
-        return len[len1][len2];       
+        return win_size;
     }
 };
 
 int main(){
     Solution a;
     string input1;
-    string input2;
-    cin >> input1 >> input2;
-    int result = a.longestCommonSubsequence(input1, input2);
-    cout << result << endl;
+    cin >> input1;
+    int len = input1.size();
+    cout << len << endl;
+    cout << input1[len];
 }
 
