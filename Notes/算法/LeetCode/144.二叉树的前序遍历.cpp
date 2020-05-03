51cbc11bbde9e68e78844a071ec83dcd2af5bbcb
胡智宇 <huzhiyu2828@gmail.com>
2020-05-04 00:44:39 +0800

    update_2020_05_04

--- Notes/算法/LeetCode/144.二叉树的前序遍历.cpp
+++ Notes/算法/LeetCode/144.二叉树的前序遍历.cpp
@@ -15,10 +15,37 @@
  * };
  */
 class Solution {
+    // 迭代
 public:
+    vector<int> res;
     vector<int> preorderTraversal(TreeNode* root) {
-
+        if(root == nullptr) return res;
+        stack<TreeNode*> m;
+        m.push(root);
+        while(!m.empty()){
+            TreeNode* ele = m.top(); // 访问栈顶元素
+            m.pop(); // 推出栈顶元素
+            res.push_back((*ele).val);
+            if(ele->right) m.push(ele->right);
+            if(ele->left) m.push(ele->left);
+        }
+        return res;
     }
 };
 // @lc code=end
+/*
+class Solution {
+    // 递归
+public:
+    vector<int> res;
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(root != nullptr){
+            res.push_back((*root).val);
+            preorderTraversal(root->left);
+            preorderTraversal(root->right);
+        }
+        return res;
+    }
 
+};
+*/
