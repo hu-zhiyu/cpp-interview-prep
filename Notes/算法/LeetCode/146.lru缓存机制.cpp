ef1b0e47500e0c4fbfb3e751a575d77238c61237
胡智宇 <huzhiyu2828@gmail.com>
2020-05-09 00:14:17 +0800

    update_2020_05_09

--- Notes/算法/LeetCode/146.lru缓存机制.cpp
+++ Notes/算法/LeetCode/146.lru缓存机制.cpp
@@ -5,34 +5,47 @@
  */
 
 // @lc code=start
+
+struct Val{
+    int value;
+    list<int>::iterator it;
+    Val(int a, list<int>::iterator iter) : value(a), it(iter){}
+};
 class LRUCache {
-public:
+private:
     int capacity;
-    unordered_map<int,int> m;
-    unordered_map<int,int> timeStamp; 
+    unordered_map<int,Val> m; // 哈希表存放指向双向链表的指针
+    list<int> l;  // 双向链表, 存key
+public:
     LRUCache(int cp) {
         capacity = cp;
     }
-    
     int get(int key) {
-        if((*this).m.find(key) != (*this).m.end()){
-            timeStamp[key]++;
-            return m[key];
+        auto it = m.find(key);
+        if(it != m.end()){ // 要找的key在缓存中
+            l.splice(l.begin(), l, it->second.it);
+            return it->second.value;
         }
         else return -1;        
     }
-    
     void put(int key, int value) {
-        if((*this).m.find(key) != (*this).m.end())
-            m[key] = value;
-        else{
-            if(m.size() < capacity) m[key] = value;
-
+        auto it = m.find(key);
+        if(it != m.end()){ // 要找的key在缓存中
+            l.splice(l.begin(), l, it->second.it);
+            it->second.value = value;
+        }
+        else{ // 没找到
+            if(m.size() == capacity){ // 缓存满了
+                m.erase(l.back());
+                l.pop_back();
+            }
+            l.push_front(key);
+            Val v(value, l.begin());
+            m.insert(std::pair<int,Val>(key,v));
         }
 
     }
 };
-
 /**
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache* obj = new LRUCache(capacity);
