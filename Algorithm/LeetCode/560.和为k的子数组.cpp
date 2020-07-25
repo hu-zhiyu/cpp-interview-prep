ff3184e0fb278dab2a900ad5c687f09935df1962
Zhiyu Hu <zhiyuhu@amazon.com>
2020-07-26 00:20:49 +0800

    update_2020-07_26

---
+++ Algorithm/LeetCode/560.和为k的子数组.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=560 lang=cpp
+ *
+ * [560] 和为K的子数组
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+
+    }
+};
+// @lc code=end
+
---
+++ Algorithm/LeetCode/90.子集-ii.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=90 lang=cpp
+ *
+ * [90] 子集 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+
